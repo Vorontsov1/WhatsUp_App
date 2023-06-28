b1ce6bda5958f25dc4d5dd0c8aeae0c37854af2a
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-06-28 12:42:09 +0300

    Users

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import Navigator from "./src/navigation";
-import {withAuthenticator} from 'aws-amplify-react-native';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
+import { withAuthenticator } from "aws-amplify-react-native";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+import { getUser } from "./src/graphql/queries";
+import {createUser} from "./src/graphql/mutations";
+
+
 
 
 Amplify.configure({
@@ -13,7 +18,46 @@ Amplify.configure({
   },
 });
 
-function App () {
+function App() {
+
+  useEffect(() => {
+    const syncUser = async () => {
+      try {
+        //get Auth userSelect:
+        const authUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+        console.log(authUser);
+        // query the database using Auth user id(sub)
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: authUser.attributes.sub })
+        );
+        console.log(userData);
+
+        if (userData.data.getUser) {
+          console.log("user already exist in DB");
+          return;
+        }
+
+        // if there is no users in db, create one
+        const newUser = {
+          id: authUser.attributes.sub,
+          name: authUser.attributes.phone_number,
+          status: "Hey, i am using What`sapp",
+        };
+
+        const newUserResponse = await API.graphql(
+          graphqlOperation(createUser, { input: newUser })
+        );
+        console.log(newUserResponse);
+      } catch (error) {
+        console.error("Error syncing user:", error);
+      }
+    };
+    syncUser();
+  }, []);
+
+
   return (
     <View
       style={{
@@ -28,4 +72,4 @@ function App () {
   );
 }
 
-export default withAuthenticator(App);   
+export default withAuthenticator(App);
